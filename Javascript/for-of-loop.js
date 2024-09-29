009a11f7f20ba3ded667012f12fe0094d0bc814d
Usman Javed <usmanjaved0816@gmail.com>
2024-09-29 17:17:41 +0500

    for off loop

---
+++ Javascript/for-of-loop.js
@@ -0,0 +1,14 @@
+    
+
+
+    // for of loop
+
+    const colors = ['white' , 'blue' , 'red'];
+
+     for (let color of colors)
+        console.log(color);
+
+
+     const names = ['amna', 'rio', 'laiba'];
+     for (let Name of names)
+        console.log (Name);
