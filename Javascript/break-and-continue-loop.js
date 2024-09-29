50b53b0a5239f910524a587341357f0578b2acf3
Usman Javed <usmanjaved0816@gmail.com>
2024-09-29 17:34:12 +0500

    continue and break statement

---
+++ Javascript/break-and-continue-loop.js
@@ -0,0 +1,13 @@
+//    break and continue loop
+let i = 0;
+while (i <= 10)
+{
+    // if (i === 5) break;
+
+    if (i % 2 === 0 ){
+        i++;
+        continue;
+    }
+    console.log (i);
+    i++;
+}
