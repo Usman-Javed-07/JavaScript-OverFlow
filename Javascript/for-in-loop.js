5f1854eb78d5d45fe5da9398c1ca5c0d83bee6f9
Usman Javed <usmanjaved0816@gmail.com>
2024-09-29 17:11:55 +0500

    for in loop in js

---
+++ Javascript/for-in-loop.js
@@ -0,0 +1,16 @@
+
+
+// for in loop 
+
+const person = {
+    Name : 'elsa',
+    Age : 5
+};
+
+for (let key in person)
+console.log (key, person[key]);
+
+
+const color = ['red', 'blue', 'black'];
+for (let index in color)
+    console.log(index, color[index]);
