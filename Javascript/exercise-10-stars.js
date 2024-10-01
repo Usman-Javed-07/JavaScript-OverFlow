2de1f78eb51355a373bcfdd30394f772a7f73f0a
Usman Javed <usmanjaved0816@gmail.com>
2024-10-01 12:13:30 +0500

    solve exercise stars

---
+++ Javascript/exercise-10-stars.js
@@ -0,0 +1,12 @@
+  // stars
+
+
+
+  showStars (1);
+  function showStars (rows) {
+    for (let i = 0 ; i <= rows; i++)
+    {
+        let str = "*";
+        console.log(str.repeat(i));
+    }
+  }
